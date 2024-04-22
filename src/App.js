2341d506d3a46d3d61b3c7b1c324c36b76d7fc43
shivanshupareek <97258879+shivanshupareek@users.noreply.github.com>
2024-04-22 14:17:37 +1000

    fsd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./style.css";
 
-function App() {
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion data={faqs} />
     </div>
   );
 }
 
-export default App;
+//how to map:
+//{OriginalArrayName.map(newArrayName => locationOfNewArray_AKA_NewComponent name={newArrayName.selectThePropertyFromTheOrigianlArray} )}
+//How to index the aforementioned new array with index numbers. The map also indexs the number along with property of the array
+//{OriginalArrayName.map(newArrayName, i => locationOfNewArray_AKA_NewComponent name={newArrayName.selectThePropertyFromTheOrigianlArray} num={i} )}
+
+function Accordion({ data }) {
+  return (
+    <div accordion>
+      {data.map((el, i) => (
+        <AccordionItem title={el.title} text={el.text} num={i} />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToogle() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+
+  return (
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToogle}>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{text}</div>}
+    </div>
+  );
+}
